0ade55aa7e74835307767c192b86b573da02e9d4
Lucas Calderon <lucas8calderon@hotmail.com>
2021-10-22 14:51:20 -0300

    first commit

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PnTela = new System.Windows.Forms.Panel();
+            this.LbPontuacao = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.iniciarJogoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Frame = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnTela
+            // 
+            this.PnTela.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PnTela.Location = new System.Drawing.Point(1, 58);
+            this.PnTela.Name = "PnTela";
+            this.PnTela.Size = new System.Drawing.Size(439, 435);
+            this.PnTela.TabIndex = 0;
+            // 
+            // LbPontuacao
+            // 
+            this.LbPontuacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbPontuacao.Location = new System.Drawing.Point(1, 20);
+            this.LbPontuacao.Name = "LbPontuacao";
+            this.LbPontuacao.Size = new System.Drawing.Size(439, 35);
+            this.LbPontuacao.TabIndex = 0;
+            this.LbPontuacao.Text = "PONTOS: 0";
+            this.LbPontuacao.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Menu});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(441, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            this.Menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarJogoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.Menu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(51, 20);
+            this.Menu.Text = "Menu";
+            // 
+            // iniciarJogoToolStripMenuItem
+            // 
+            this.iniciarJogoToolStripMenuItem.Name = "iniciarJogoToolStripMenuItem";
+            this.iniciarJogoToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.iniciarJogoToolStripMenuItem.Text = "Iniciar Jogo";
+            this.iniciarJogoToolStripMenuItem.Click += new System.EventHandler(this.iniciarJogoToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Frame
+            // 
+            this.Frame.Tick += new System.EventHandler(this.Frame_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(441, 498);
+            this.Controls.Add(this.LbPontuacao);
+            this.Controls.Add(this.PnTela);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SNAKE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Clicado);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnTela;
+        private System.Windows.Forms.Label LbPontuacao;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Menu;
+        private System.Windows.Forms.ToolStripMenuItem iniciarJogoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Timer Frame;
+    }
+}
+
---
+++ snake/Game.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake
+{
+    class Game
+    {
+        public Keys Direction { get; set; }
+        public Keys Arrow { get; set; }
+
+        private Timer Frame { get; set; }
+        private Label LbPontuacao { get; set; }
+        private Panel Pntela { get; set; }
+
+        private int pontos = 0;
+
+        private Food Food;
+
+        private Snake Snake;
+
+        private Bitmap offScreenBitmap;
+
+        private Graphics bitmapGraph;
+
+        private Graphics screenGraph;
+
+        public Game(ref Timer timer, ref Label label, ref Panel panel)  {
+
+
+
+            Pntela = panel;
+            Frame = timer;
+            LbPontuacao = label;
+            offScreenBitmap = new Bitmap(428, 428);
+            Snake = new Snake();
+            Food = new Food();
+            Direction = Keys.Left;
+            Arrow = Direction;
+            
+
+
+
+        }
+
+        public void StartGame() {
+
+            Snake.Reset();
+            Food.CreateFood();
+            Direction = Keys.Left;
+            bitmapGraph = Graphics.FromImage(offScreenBitmap);
+            screenGraph = Pntela.CreateGraphics();
+            Frame.Enabled = true;
+
+
+
+        }
+
+        public void Tick()
+        {
+            if (((Arrow == Keys.Left) && (Direction != Keys.Right)) ||
+
+
+           ((Arrow == Keys.Right) && (Direction != Keys.Left)) ||
+
+           ((Arrow == Keys.Up) && (Direction != Keys.Down)) ||
+
+           ((Arrow == Keys.Down) && (Direction != Keys.Up)))
+
+            {
+                Direction = Arrow;
+
+            }
+
+            switch (Direction)
+            {
+                case Keys.Left:
+                    Snake.Left();
+                    break;
+
+                case Keys.Right:
+                    Snake.Right();
+                    break;
+
+                case Keys.Up:
+                    Snake.Up();
+                    break;
+
+                case Keys.Down:
+                    Snake.Down();
+                    break;
+
+            }
+
+            bitmapGraph.Clear(Color.White);
+
+            bitmapGraph.DrawImage(Properties.Resources.RAT, (Food.Location.X * 15), (Food.Location.Y * 15), 15, 15);
+
+
+            bool gameOver = false;
+
+
+
+            for (int i = 0; i < Snake.Lenght; i++) {
+
+                if (i == 0) {
+
+                    bitmapGraph.FillEllipse(new SolidBrush(ColorTranslator.FromHtml("#000000")), (Snake.Location[i].X * 15), (Snake.Location[i].Y * 15), 15, 15);
+                }
+
+                else
+                {
+                    bitmapGraph.FillEllipse(new SolidBrush(ColorTranslator.FromHtml("#4F4F4F")), (Snake.Location[i].X * 15), (Snake.Location[i].Y * 15), 15, 15);
+                }
+
+
+                if ((Snake.Location[i] == Snake.Location[0]) && (i > 0)) {
+                    gameOver = true;
+
+                }
+            }
+
+             screenGraph.DrawImage(offScreenBitmap, 0, 0);
+            CheckCollision();
+
+            if (gameOver) {
+                GameOver();
+            }
+        }
+
+        public void CheckCollision() {
+            if (Snake.Location[0] == Food.Location)
+            {
+                Snake.Eat();
+                Food.CreateFood();
+                pontos += 10;
+                LbPontuacao.Text = "PONTOS: " + pontos;
+            }
+
+
+        }
+    
+
+            
+
+
+        public void GameOver() {
+            Frame.Enabled = false;
+            bitmapGraph.Dispose();
+            screenGraph.Dispose();
+            LbPontuacao.Text = "PONTOS: 0";
+            pontos = 0;
+            MessageBox.Show("Game Over");
+
+
+        }
+
+    }
+}
+
+
+
+
+
+            
+
+            
+       
+   
+
+
+
